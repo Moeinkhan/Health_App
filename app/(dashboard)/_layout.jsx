869a25a74d568d455bd35649b89196f4b4016a00
Moein <moeinkhanmohamadi3@gmail.com>
2026-01-01 22:33:08 +0330

    TabBar layout changed

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,8 +1,28 @@
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 const DashboardLayout = () => {
   return (
-    <Tabs />
+    <Tabs screenOptions={{headerShown: false, tabBarStyle: {height: 60}}}>
+      <Tabs.Screen
+        name="index" 
+        options={{title: 'Home', tabBarIcon: ({focused}) => (
+          <Ionicons
+            size={24}
+            name={focused ? "home" : "home-outline"}
+          />
+        )}}
+      />
+      <Tabs.Screen 
+        name="history" 
+        options={{title: 'History', tabBarIcon: ({focused}) => (
+          <Ionicons
+            size={24}
+            name={focused ? "time" : "time-outline"}
+          />
+        )}}
+      />
+    </Tabs>
   )
 }
 
